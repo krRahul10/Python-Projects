b72285484940d2259bce903e59317dcb32df6495
Rahul <rahul10kr7@gmail.com>
2026-01-26 17:44:53 +0530

    conditions statement

---
+++ Pythons-Basic/Conditions.py
@@ -0,0 +1,42 @@
+# RedLight Example
+
+'''light = input("light :")
+if(light == 'red'):
+    print('STop')
+elif(light == 'green'):
+    print('Goo!')
+elif(light == 'yellow'):
+    print("Look")
+else:
+    print('Light broken')'''
+
+# School Grade system
+
+'''marks = int(input("Enter your marks"))
+if (marks >= 90):
+    print("A+ Grade")
+elif(marks >= 80 and marks <=90):
+    print('B+ Grade')
+elif(marks >= 50 and marks <=80 ):
+    print("C+ grade")
+else:
+    print('only Pass')'''
+    
+
+# Single Line Condition statement (Ternary Operator)
+
+# food = input("Food : ")
+# eat = "Yes" if food == "cake" else 'no'
+# print(eat)
+
+# food = input ("food : ")
+# print("SWeet") if food == "Cake" or food == "Jalebi" else print("No sweet")
+
+
+# age = int(input("Age : "))
+# vote = ("Yes", "No") [age >= 18]
+# print(vote)
+
+sal = int(input("Salary : "))
+tax = sal*(0.1, 0.3) [sal >= 50000]
+print(tax)
