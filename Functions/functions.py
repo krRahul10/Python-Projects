e27c3b5a35c020ea9f80593c8d8b8ff1a3d5ea92
Rahul <rahul10kr7@gmail.com>
2026-01-28 14:15:41 +0530

    conditions

---
+++ Functions/functions.py
@@ -0,0 +1,17 @@
+str = "i am studying in Masai school"
+print(str.endswith("ool"))
+print(str.capitalize())
+
+# Replace function
+
+# print(str.replace("o","j"))
+print(str.find("s"))
+
+# count function
+
+print(str.count("o"))
+
+name = "Rahul K$$$$$$umar"
+print(len(name))
+
+print(name.count("$"))
--- Pythons-Basic/Conditions.py
+++ Pythons-Basic/Conditions.py
@@ -37,14 +37,57 @@ else:
 # vote = ("Yes", "No") [age >= 18]
 # print(vote)
 
-sal = int(input("Salary : "))
-tax = sal*(0.1, 0.3) [sal >= 50000]
-print(tax)
+# sal = int(input("Salary : "))
+# tax = sal*(0.1, 0.3) [sal >= 50000]
+# print(tax)
 
 # Assignment operator
 
-num = 10
-num = num 
-num= num**5
-print(num)
+# num = 10
+# num = num 
+# num= num**5
+# print(num)
+
+# Nesting condition
+
+# age = int(input("Enter your age :"))
+
+# if (age >= 18):
+#     # print(" You can drive")
+#     if(age >=80):
+#         print("You can not drive bhude ghar ja")
+#     else:(print("Can drive"))
+# else:
+#     print("app abi bache ho")
+    
+    
+# write a programme check if a number entered by user is odd or even
+
+# number = int(input("Enter your number to check odd or even :"))
+
+# if(number%2==0):
+#     print("even")
+# else:
+#     print("odd")    
+    
+# write a programme to check if a number is multiple by 7 or not
+
+# number = int(input("Enter your number to check odd or even :"))
+
+# if(number%7==0):
+#     print("Can multiple by 7")
+# else:
+#     print("can't multiple by 7")  
+    
+# write programme to find the greatest number enter by user   
+
+number1 = int(input("Enter your number1 :"))
+number2 = int(input("Enter your number2 :"))
+number3 = int(input("Enter your number3 :"))
+
+if (number1 > number2 and number1 > number3):
+    print("A is greater than B and C ", number1)
+elif(number2 > number3):
+    print("B is greater than C and A", number2)
+else: print("C is greater than A and B",number3)
 
